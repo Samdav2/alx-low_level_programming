da3b198563901772d533c80da74b667428dd1cb6
Samdav2 <dawoduJohn1@gmail.com>
2023-02-17 05:16:29 -0800

    script that prints the c program in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programmig is like building a multilingual puzzle\n");
+	puts("%d\"Programmig is like building a multilingual puzzle\n");
 	return (0);
 
 }
