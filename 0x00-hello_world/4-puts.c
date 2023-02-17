5c910ffb7432465b01614bdf76ee601502ba3c34
Samdav2 <dawoduJohn1@gmail.com>
2023-02-17 06:28:19 -0800

    script that prints the c program in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,9 @@
  * main - This returns no arguements
  */
 int main(void)
+/**
+ * puts - Prints programming is like building a multilingual puzzle
+ */
 {
 	puts("\"Programmig is like building a multilingual puzzle\n");
 	getchar();
