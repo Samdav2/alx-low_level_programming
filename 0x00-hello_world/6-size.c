bed30ad74e1b2cb1b32be4be525af210de62d44d
Samdav2 <dawoduJohn1@gmail.com>
2023-02-18 15:00:53 -0800

    C program that prints negative and positive numbers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+}
+int i;
+char c;
+long int t;
+long long int l;
+float f;
+	printf("Size of a char: %zu bytes\n", sizeof(c));
+	printf("Size of an int: %zu bytes\n", sizeof(i)); 
+	printf("Size of a long int: %zu bytes\n", sizeof(t));
+	printf("Size of a long long: %zu bytes\n", sizeof(l));
+	printf("Size of a float: %zu bytes\n", sizeof(f));
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This README  file contains all the information of all the c file in this folder 
