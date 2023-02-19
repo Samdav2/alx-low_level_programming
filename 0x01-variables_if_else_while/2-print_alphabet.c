4777aa3423d3a333d9fa4168ea0a91166b2a6064
Samdav2 <dawoduJohn1@gmail.com>
2023-02-18 17:01:34 -0800

    C program that prints letter from a-z in smaller case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program takes in no arquement
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
