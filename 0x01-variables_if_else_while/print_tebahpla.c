646b4f18e2c48ed20a53ca8b37402aebed56f5c4
Samdav2 <dawoduJohn1@gmail.com>
2023-02-19 08:49:23 -0800

    C program that prints program in backward form

---
+++ 0x01-variables_if_else_while/print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This section takes in no arguement
+ * Return:0(success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter--)
+		{
+		putchar(letter);
+		}
+		putchar('\n');
+		return (0);
+}
+
