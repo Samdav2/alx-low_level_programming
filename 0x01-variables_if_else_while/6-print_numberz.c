a3cd463804294144c6be73e0edef86c038aea932
Samdav2 <dawoduJohn1@gmail.com>
2023-02-19 08:12:08 -0800

    C program that prints numbers from 0 to 9 with putcar keyword

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This indentifier does not take in any arquements
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number = 0; number < 10; number++)
+		{
+		putchar(number);
+		}
+		putchar('\n');
+		return (0);
+}
