66a2b6ec22e10d1b88ab75902aeea67673774a8b
Samdav2 <dawoduJohn1@gmail.com>
2023-02-19 07:45:26 -0800

    C program that prints letters from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This section does not accept arguements
+ * Return:0(success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+		{
+		printf("%d\n", number);
+		}
+	return (0);
+}
