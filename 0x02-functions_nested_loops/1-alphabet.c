521e1f402b253c0f7ab581ddb1a361699db8d866
Samdav2 <dawoduJohn1@gmail.com>
2023-02-22 09:34:36 -0800

    Fixing prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
  * main - prints letter a-z is smaller cases
  * Return: 0(success)
  */
+void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - prints letter a-z is smaller cases
  *Return: 0(success)
