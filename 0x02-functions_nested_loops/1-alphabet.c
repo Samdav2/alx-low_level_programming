5d563339e48e161c69254b79d6e7e0744868b973
Samdav2 <dawoduJohn1@gmail.com>
2023-02-21 16:23:24 -0800

    C programm that prints letter from a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints letter a-z is smaller cases
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		putchar(letter);
+		}
+		putchar('\n');
+		return (0);
+}
