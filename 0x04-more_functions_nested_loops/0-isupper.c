66b976fbd4d6f3698c6220b21465b23e609ab718
Samdav2 <dawoduJohn1@gmail.com>
2023-02-23 07:47:02 -0800

    C Function that differentiate between capital and small letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isupper - Run one if c is upper and zero is c is not 
- * Return: (0);
- * @c: chaacter for holding letters
+ * _isupper - Run one if c is upper and zero is c is not
+ * Return:0(success)
+ * @c:chaacter for holding letters
  */
 int _isupper(int c)
 {
-	if( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 	return (1);
 	}
