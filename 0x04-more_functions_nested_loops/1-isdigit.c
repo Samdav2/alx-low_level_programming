c8b7ea1388592d885d2dc95c790226ee1e725972
Samdav2 <dawoduJohn1@gmail.com>
2023-02-23 14:27:21 -0800

    C program that returns 1 if c is digit and 0 if it is not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - prints 1 if the number is digit and 0 if not
+ * Return: 0
+ * @c: Use for determining character
+ */
+int _isdigit(int c)
+{
+	c = 'a';
+	
+	if (c != 0 && c != 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@
  */
 int _isupper(int c);
 int _putchar(char c);
+int _isdigit(int c);
