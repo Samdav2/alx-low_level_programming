28124190b2c5cd2703e69c7dad7417eb66cd1d46
Samdav2 <dawoduJohn1@gmail.com>
2023-02-22 19:06:50 -0800

    C program that prints 1 is c is lower and 0 if it is not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Function that checks for lowercas character
+ * Return: 0(success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
