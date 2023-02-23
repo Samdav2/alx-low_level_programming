b97c00674e3359c7216ce43baf3e854392688e8d
Samdav2 <dawoduJohn1@gmail.com>
2023-02-23 04:47:09 -0800

    c program that test the function positive or negative with value o

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Test functions that prints integer positive or negative 
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This files explains what each program fuction files will be performing 
