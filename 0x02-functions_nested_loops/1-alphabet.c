e9331e71ccf23b1883c8519446db46a355100057
Samdav2 <dawoduJohn1@gmail.com>
2023-02-22 18:35:32 -0800

    Hot correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - prints letter a-z is smaller cases
+ * print_alphabet - prints letter a-z is smaller cases
  * Return: 0(success)
  */
 void print_alphabet(void)
 {
 	char letter;
-	letter = 'a';
 
+	letter = 'a';
 	while (letter <= 'z')
 	{
 	_putchar(letter);
