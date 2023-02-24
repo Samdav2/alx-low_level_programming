04ab83e9c82960586f0afa5f4853c9a22048dde1
Samdav2 <dawoduJohn1@gmail.com>
2023-02-24 00:08:59 -0800

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,12 @@ void more_numbers(void)
 	{
 	for (a = 0; a <= 14; a++)
 		{
-		_putchar(n);
+		if (a > 9)
+		{
+		_putchar((a / 10) +'0');
 		}
-		_putchar('\n');
+		_putchar((a % 10) + '0');
+		}
+		_putchar(10);
 	}
 }
