f2f4fc4a420cd1067a9546bf9b06d07bb1c2f16a
Samdav2 <dawoduJohn1@gmail.com>
2023-02-24 02:47:23 -0800

    C program that display triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - functions that print triangles
+ * @size: Hold a vlues
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > i; j--)
+			{
+			_putchar(' ');
+			}
+			for (k = 1; k <= j; k++)
+			{
+			_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+}
