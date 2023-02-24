059c7ed8b36347078e59f2fb59fc1de6d8571394
Samdav2 <dawoduJohn1@gmail.com>
2023-02-24 15:21:52 -0800

    Debugging the debugging files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - Test functions that prints integer positive or negative 
+ * main - Test functions that prints integer positive or negative
  * Return: 0(success)
  */
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 
 	positive_or_negative(i);
