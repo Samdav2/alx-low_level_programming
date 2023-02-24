8180846707875a41f72908c49eefc97c6b56709a
Samdav2 <dawoduJohn1@gmail.com>
2023-02-24 01:03:44 -0800

    C function that square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - Function that print square
+ * Returns: Nothing
+ * @size: size of a square in row/col units
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
