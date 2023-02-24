0a69d1744e35bb3f03e8993b9be2c96d7ad430f0
Samdav2 <dawoduJohn1@gmail.com>
2023-02-23 23:34:25 -0800

    Hot Fix

---
+++ 0x04-more_functions_nested_loops/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - functions for printing numbers
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for  (n = 48; n < 58; n++)
+	{
+	_putchar(n);
+	}
+	_putchar(10);
+}
