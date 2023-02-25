ab081e24dd1c97abdc31f24382810f8e6950688b
Samdav2 <dawoduJohn1@gmail.com>
2023-02-24 19:48:15 -0800

    Fixing infinite looping

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Fixing ad infinite loop
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	prinnf("Infinite loop incoming :(\n");
+
+	i = 0
+	/*
+	 * while (i > 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Ifinite loop avoided! \\o/\n");
+
+	return (0);
+}
