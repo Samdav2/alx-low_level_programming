85519cd52c322561c4fa1ba5f462612febb89490
Samdav2 <dawoduJohn1@gmail.com>
2023-02-26 15:32:57 -0800

    C functions that prints

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_bauer - clock that prints every three minute of the day 
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar((a) + '0');
+	_putchar((b) + '0');
+	_putchar(':');
+	_putchar((c) + '0');
+	_putchar((d) + '0');
+	}
+
+}
