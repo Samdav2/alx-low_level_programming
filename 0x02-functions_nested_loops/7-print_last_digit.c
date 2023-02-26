44c81692dbf60682820291f0eeed9bf0c066b9ec
Samdav2 <dawoduJohn1@gmail.com>
2023-02-25 22:00:17 -0800

    C function that prints the last function of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * Return: Always zero 0
+ * @n: Hold a vlaue
+ * @last_digit: Holds a specific value
+ */
+int print_last_digit(int n)
+{
+	int last_digit
+
+	last digit = n % 10;
+	_putchar((last_digit) + '0');
+	return (0);
+}
