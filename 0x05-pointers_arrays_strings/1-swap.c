552dc1d938d51ea3177207786a64f3ada96e5b4d
Samdav2 <dawoduJohn1@gmail.com>
2023-02-27 08:31:56 -0800

    C function that replace the value of two integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the value of two integers
+ * Return: Nothing
+ * @a: Holds the address of another integer
+ * @b: Holds the address of another integer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+}
