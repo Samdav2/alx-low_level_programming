725d55c56044390b38dbcbf4c35275402cdc5a6a
Samdav2 <dawoduJohn1@gmail.com>
2023-02-26 22:06:38 -0800

    C function that adds two value together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - C function that adds two integer together
+ * @a: Holds a value
+ * @b: Holds a value
+ * Return: 0(success)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
