a52f955dd60dd3c7e21ca12e01038f7ad297f55c
Samdav2 <dawoduJohn1@gmail.com>
2023-02-28 00:41:11 -0800

    C functions that generate password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-/**
- * main - generate password
- * Return: nothing
- */
+#include <time.h>
+
+/** 
+ *  *main- genrate password
+ *   *Return:nothing
+ *    */
 int main(void)
 {
-	srand((unsigned int) time(NULL));
+	srand((unsigned int)time(NULL));
 	char pass[13];
 	int i;
 
+
 	for (i = 0; i < 4; i++)
 	{
-	pass[3 * i] = '0' + (rand() % 10);
-	char capLetter = 'A' + (rand () % 26);
 
-	pass[(3 * i) + 1] = capLetter;
-	char Letter = 'a' + (rand() % 26);
+		pass[3 * i] = '0' + (rand() % 10);
+		char capLetter = 'A' + (rand() % 26);
 
-	pass[( 3 * i) + 2] = letter;
-	pass[3 * i] = "10";
+		pass[(3 * i) + 1] = capLetter;
+		char letter = 'a' + (rand() % 26);
+
+		pass[(3 * i) + 2] = letter;
+	}
+	pass[3 * i] = '\0';
 	printf("generated password : %s\n\n", pass);
 
 	printf("\n\n");
 	return (0);
-	}
 }
