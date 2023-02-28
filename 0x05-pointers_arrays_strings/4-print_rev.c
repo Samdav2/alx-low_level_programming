f459985d1c5294a873266c825ea3de3a03948926
Samdav2 <dawoduJohn1@gmail.com>
2023-02-28 13:58:44 -0800

    C funtion that recerse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,21 @@
-#include "main.h"
+nclude "main.h"
 /**
- * print_rev - C function that prints in a reverse way
- * R return: Nothing
- * @s: holds the input value
+ * print_rev - prints a string, in reverse, followed
+ * @s: input string.
+ * Return: no return.
  */
-void print_rev(char *str)
+
+void print_rev(char *s)
 {
-	puts(str);	
+	int count = 0;
+
+	while (count >= 0)
+	{
+	if (s[count] == '\0')
+	break;
+	count++;
+	}
+	for (count--; count >= 0; count--)
+	_putchar(s[count]);
+	_putchar('\n');
 }
