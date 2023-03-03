16934717c2c415f66847e3978dcc5e425c3d54a3
Samdav2 <dawoduJohn1@gmail.com>
2023-03-03 06:49:32 -0800

    C function that concrecrate specific value of character

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - C function that appen da specific number of string to
+ * another string
+ * @dest: A pointer for a string
+ * @str: A pointer to a string
+ * @n: Hols a value to print specific amount of string
+ * Return: A character or strings
+ */
+char *_strncat(char *dest, char *str, int n)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (*(dest + a ) != '\0')
+	{
+	a++;
+	}
+
+	while (*(str + b) <= n)
+	{
+	*(dest + a) = *(str + b);
+	if (*(str + b) == '\0')
+	break;
+	a++;
+	b++;
+	}
+	return (dest);
+}
