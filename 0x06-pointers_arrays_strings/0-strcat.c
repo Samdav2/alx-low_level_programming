237e082dc55c311ed22e2f5e96b10d8387a5c09e
Samdav2 <dawoduJohn1@gmail.com>
2023-03-03 04:30:57 -0800

    C program that append strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int a , b;
 
+	a = 0;
+	b = 0;
+
+	while (*(dest + a) != '\0')
+	{
+	a++;
+	}
+
+	while (b >= 0)
+	{
+	*(dest + a) = *(src + b);
+	if (*(src + b) == '\0')
+	break;
+	a++;
+	b++;
+	}
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 /**
  * This file contain all the prototype of the c function files in the directory
  */
+int main();
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+
+#endif
