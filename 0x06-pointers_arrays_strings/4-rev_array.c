7083ef972c4f1edda67466ed404b1c9de9c55eb2
Samdav2 <dawoduJohn1@gmail.com>
2023-03-03 17:48:37 -0800

    C program that reverse a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - Reverse integer sirings
+ * @a: Holds an array of integer
+ * @n: Holds a value
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int j, i, tmp;
+
+	j = n -1;
+
+	for (i - 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+	}
+
+}
