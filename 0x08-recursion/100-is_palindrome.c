591456cf58435418664ad4ed88f85ddfe4861dda
Samdav2 <dawodujohn1@gmail.com>
2023-03-09 20:09:06 +0100

    C function that check if a letter is a palidrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,22 +4,22 @@
  * @s: Holds a string
  * Return: An integer
  */
-int is_palidrome(char *s)
+int is_palindrome(char *s)
 {
 	int column;
 
 	if (*s != '\0')
 	{
-		is_palidrome(s + 1);
+		is_palindrome(s + 1);
 		_putchar(*s);
 		_putchar('\n');
 	}
 
 	if (*s == '\0')
 	_putchar(*s);
-	is_palidrome(s + 1 );
+	is_palindrome(s + 1 );
 
-	column = is_palidrome(s + 1);
+	column = is_palindrome(s + 1);
 
 	if (*s == column)
 	return (1);
