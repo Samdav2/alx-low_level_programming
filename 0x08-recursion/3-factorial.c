2bf9bf2dee6cd6727ca824bf4c165e3f9cf510ac
Samdav2 <dawoduJohn1@gmail.com>
2023-03-08 17:46:44 -0800

    C program that prints the factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,16 +2,19 @@
 /**
  * factorial - Function that calculate the factorial of a number
  * @n:  Recieves an integer
- * Return; Return an integer
+ * Return: Return an integer
  */
 int factorial(int n)
 {
 	if (n < 0)
-
+	{
 	return (-1);
+	}
 
+	if (n == 0 || n == 1)
+	{
+	return (1);
+	}
 
-	n = n * factorial(n - 1);
-
-	return (n);
+	return (n * factorial(n - 1));
 }
