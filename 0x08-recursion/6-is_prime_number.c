e925222daa49d670733a79e2dbc21e90d132a981
Samdav2 <dawodujohn1@gmail.com>
2023-03-09 10:19:37 +0100

    C function that print prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * is_prime_number
+ * is_prime_number - Print the print 
  * @n: Holds a number
  * Return: An integer
  */
@@ -9,10 +9,12 @@ int is_prime_number(int n)
 	if (n % n == 1 && n % 1 == n)
 	{
 		if (n % 2 == 1 && n % 3 == 2)
+		{
 		return (1);
+		}
 	}
 
-	else 
+	else
 	return (0);
 
 	return (is_prime_number(n + 1));
