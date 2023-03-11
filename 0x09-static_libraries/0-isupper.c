efa5c2fd3d676c4dea57cc0fe695f8abaa0b4189
Samdav2 <dawodujohn1@gmail.com>
2023-03-11 22:06:43 +0100

    C program that adds files to libaries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Run one if c is upper and zero is c is not
+ * Return:0(success)
+ * @c:chaacter for holding letters
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+	return (1);
+	}
+	return (0);
+}
