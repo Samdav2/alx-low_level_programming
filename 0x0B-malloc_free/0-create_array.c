64cf9f4f401f4408008200a09b64292a3c3c9693
Samdav2 <dawodujohn1@gmail.com>
2023-03-14 01:52:53 +0100

    C function that creates a array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -29,11 +29,11 @@ char *create_array(unsigned int size, char c)
 
 	while (i < size)
 	{
-	print_array_address(n + 0);
+	print_array_address(&n[0]);
 	}
 	return (n);
 }
 void print_array_address(char n[])
 {
-	printf("0x%02x ", (unsigned int) n);
+	printf("%p ", n);
 }
