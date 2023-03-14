feabdb42fc5de39a5717bbc77b86e35ce33583f0
Samdav2 <dawodujohn1@gmail.com>
2023-03-14 22:33:29 +0100

    C program that concantenates two string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * str_concat - Concantenates two string
+ * @s1: First String
+ * @s2: second string
+ * Return: A string or character
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i;
+	unsigned int j;
+	unsigned int k = 0;
+
+	if (s1 == NULL || s2 == NULL)
+	putchar('\0');
+
+	for (i = 0; s1[i] = '\0'; i++)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			k++;
+		}
+	}
+
+	a = malloc((k + 1) * sizeof(char));
+	a[k] = s1[i] + s2[j];
+	free(a);
+	return (a);
+
+	
+}
