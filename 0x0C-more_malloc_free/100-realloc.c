79c3e75003033028ba2531fcf19397af1132708f
Samdav2 <dawodujohn1@gmail.com>
2023-03-21 11:15:26 +0100

    C program that rellallocates a memory block using malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * _realloc - Function that reallocate a memory
+ * ptr: pointer
+ * old_size: The memory size
+ * new_size: The new memory size
+ * Return: Nothing
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int *ptr2;
+	unsigned int i, j;
+
+	if (old_size == new_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr2 = malloc(new_size * sizeof(char));
+			if (ptr2 == NULL)
+				return (NULL);
+
+	if (new_size > old_size)
+	{
+
+
+		for (i = 0; i < old_size; i++)
+		{
+			ptr2[i] = ((char *)ptr)[i];
+		}
+		free(ptr2);
+		return (ptr2);
+	}
+
+	else
+	{
+		for(j = 0; j < new_size; j++)
+		{
+			ptr2[j] = ((char *)ptr)[j];
+		}
+		free (ptr2);
+		return (ptr2);
+	}
+}
