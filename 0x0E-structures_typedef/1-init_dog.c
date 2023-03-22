d3602135cce092701e2a7c0f7020ef0fdcbb1e42
Samdav2 <dawodujohn1@gmail.com>
2023-03-22 21:36:28 +0100

    C program that initialize the varisble type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct d;
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
