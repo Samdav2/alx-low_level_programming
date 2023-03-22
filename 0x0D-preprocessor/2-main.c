c4dafe8e06fd8aefa403ce4d901acf6eaa616d91
Samdav2 <dawodujohn1@gmail.com>
2023-03-22 14:44:35 +0100

    C program that get where a file is compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,10 @@
-#ifndef __BASE_FILE__
-#define __BASE_FILE__
-
-#endif
+#include "main.h"
+/**
+ * main - program that prints the name of the functions it was print in
+ * Return: Success
+ */
+int main(void)
+{
+	printf(__BASE_FILE__);
+	return(0);
+}
