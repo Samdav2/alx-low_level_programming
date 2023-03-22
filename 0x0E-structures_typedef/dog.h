abf0440df1ab1fd759e50830c389867dc6c9bdaa
Samdav2 <dawodujohn1@gmail.com>
2023-03-22 22:56:44 +0100

    Struct Nick Name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @age: Age of the dog
  * @owner: Owner of the dog
  */
-typedef struct dog dog_t
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}; dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
