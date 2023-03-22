fa1e381a9527c59b32df4a5a35bef698ed7ff4b2
Samdav2 <dawodujohn1@gmail.com>
2023-03-22 21:18:50 +0100

    C program that initialize the varisble type

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * init_dog - Initialise a variable type
+ * @d: The pointer
+ * @name: The dog name
+ * @age: The dog name
+ * @owner: The dog owner
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
