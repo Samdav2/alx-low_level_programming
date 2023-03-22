f07ab676fd8303b8d412584ced0d7155e5cf5a3a
Samdav2 <dawodujohn1@gmail.com>
2023-03-22 22:41:59 +0100

    C program that print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,23 +10,22 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-		{
+
 			printf("Name: %s\n", d->name);
-		}
-		else 
+		else
 			printf("Name: (nil)\n");
 
 		if (d->age > 0)
-		{
+
 			printf("Age: %.06f", d->age);
-		}
+
 		else 
 			printf("Age: (nil)\n");
 
 		if (d->owner)
-		{
+
 			printf("Owner: %s\n", d->owner);
-		}
+
 		else 
 			printf("Owner: (nil)\n");
 	}
