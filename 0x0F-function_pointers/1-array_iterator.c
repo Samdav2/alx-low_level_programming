9949a70a5399c98a0eeec8c422b61dbe13efb4f7
Samdav2 <dawodujohn1@gmail.com>
2023-03-26 23:54:31 +0100

    C program that execute other function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include <stddef.h>
 #include "function_pointers.h"
 /**
- * array_iterator -  A execute a function given 
+ * array_iterator - A execute a function given
  * @array: The array
  * @size: The size of the array
  * @action: pointer to the
  * Return: Nothing
  */
-void array_iterator(int *array,  site_t size, void(*action)(int))
+void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t i;
 
