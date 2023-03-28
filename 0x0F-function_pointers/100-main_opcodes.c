e978a6db59df73c789e1988f3290bde438834a32
Samdav2 <dawodujohn1@gmail.com>
2023-03-28 01:58:34 +0100

    C program that prints the opcodes of it own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that prints opcodes of it own main function
+ * @argc: arguement counter
+ * @argv: Vector
+ * Return: Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+	printf("Error\n");
+	return (2);
+	}
+
+	unsigned char *main_addr = (unsigned char *)main;
+	for (int i = 0; i < num_bytes; i++)
+	{
+	printf("%02x ", *(main_addr + i));
+	}
+	printf("\n");
+
+	return (0);
+}
