c50b2dbef2dbac8b5187dec63472135f55aec9be
Samdav2 <dawodujohn1@gmail.com>
2023-04-01 00:37:49 +0100

    C program that returns number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - Returns the number of elements linked
+ * @h: Pointer to element
+ * Return: Number of elements linked
+ */
+size_t list_len(const list_t *h)
+{
+	int k = 0;
+
+	while (*h)
+	{
+		list_t->next
+		l++
+	}
+
+	return(k);
+}
