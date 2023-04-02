2e65987caea0b1230fdf92bdf215560adbe4c607
Samdav2 <dawodujohn1@gmail.com>
2023-04-02 14:23:41 +0100

    C program that returns 1 if a string is palidrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,53 @@
 #include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
 /**
- * is_palindrome - search for string that are palindrome
- * @s: Holds a string
- * Return: An integer
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
  */
-char print_string(char *s)
+int find_strlen(char *s)
 {
+	int len = 0;
 
-	if (*s != '\0')
+	if (*(s + len))
 	{
-		is_palindrome(s + 1);
+		len++;
+		len += find_strlen(s + len);
 	}
-	return (is_palindrome(s + 1));
+	return (len);
 }
-
-char print_reverse(char *s)	
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
 {
-
-	if (*s == '\0')
-	{
-	is_palindrome(s + 1);
-	}
-	return (is_palindrome(s + 1));
+	if (s[index] == s[len / 2])
+		return (1);
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+	return (0);
 }
-
-char print_reverse(char *s);
-char print_strin(char *s);
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
+ */
 int is_palindrome(char *s)
 {
+	int index = 0;
+	int len = find_strlen(s);
 
-	if (print_string(s) == print_reverse(s))
-	{	
-	return (1);
-	}
-
-	else
-	return (0);
+	if (!(*s))
+		return (1);
+	return (check_palindrome(s, len, index));
 }
