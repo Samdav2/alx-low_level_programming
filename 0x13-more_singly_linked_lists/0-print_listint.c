51ae12c1188cdf4f4c9f44928d81bf5e965b4982
Samdav2 <dawodujohn1@gmail.com>
2023-04-03 23:42:27 +0100

    C program that list all the element of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - Function that print the elements of a list
+ * @: A pointer to head of the list
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This File contains all the information for each c file in this directory
