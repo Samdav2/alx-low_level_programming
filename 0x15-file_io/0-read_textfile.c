925312d6b079a1c7e3a44a105b45b509b4f5dcd8
Samdav2 <dawodujohn1@gmail.com>
2023-04-09 04:25:42 +0100

    C program that read and write a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * read_textfile - C functions that opens and read a text file
+ * @filename: A pointer to the file to be open and read
+ * @letters: The size or numbers of letters on the file
+ *
+ * Return: The number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t read;
+
+	size_t write;
+
+	char *buffer;
+
+	FILE *ptr;
+
+	if (filename == NULL)
+
+		return (0);
+	
+	ptr = fopen(filename, "r");
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters+ 1);
+
+	if (buffer == NULL)
+	{
+		fclose(ptr);
+
+		return (0);
+	}
+
+	read = fread(buffer, sizeof(char), letters, ptr);
+
+	if (read < 0)
+	{
+		read = 0;
+	}
+
+	write = fwrite(buffer, sizeof(char), read, stdout);
+
+	if (write != read)
+	{
+	 read = 0;
+	}
+
+	free(buffer);
+
+	fclose(ptr);
+
+	return (read);
+
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Yhis file contains all the functions of all the c file in this directory
