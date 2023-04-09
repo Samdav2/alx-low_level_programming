72831154868ff80d4e3c8b0dfd49d0f513812f84
Samdav2 <dawodujohn1@gmail.com>
2023-04-09 19:02:39 +0100

    C program the create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	
+
 	int i, w, ptr;
 
 
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		text_content = "";
-	
+
 	for (i = 0; *(text_content + i); i++)
 		;
 
