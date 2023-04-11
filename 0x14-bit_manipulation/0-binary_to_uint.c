bf2e76ca54c933946ae53785c4a018e4b9bfa88c
Samdav2 <dawodujohn1@gmail.com>
2023-04-11 07:04:48 +0100

    C program that converts binary to unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	 for (i = 0; b[i] != '\0'; i++)
 	{
-		if (atoi(int)(b[i]) != 0 && atoi(int)(b[i]) != 1)
+		if (b[i] != '0' && b[i] != '1')
 
 			return (0);
 	}
