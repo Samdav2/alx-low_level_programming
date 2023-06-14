c805b67551255b764d18969a5891298b7e9f025a
Samdav2 <dawodujohn1@gmail.com>
2023-06-14 19:51:44 +0100

    The program that print the number of arguemwnt

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - fuction that prints the number of arguement passed into it
+ * @argc: Arguement counter
+ * @argv: The arguement vector
+ * Return: The solution
+ */
+int main(int argc, char *argv[])
+{
+		printf("%d\n", argc);
+
+	(void)argv;
+
+	return (0);
+}
