cfac84b430ee832b71087b0449f81264e1633bd5
Samdav2 <dawodujohn1@gmail.com>
2023-06-14 09:39:13 +0100

    c Pprogram that print the file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - C function that prints the file name
+ * @argc: It count the  number of arguement
+ * @argv: The vector or the arguement
+ * Return: the file name
+ */
+int main(int argc, char *argv[])
+{
+	while (argc = 0)
+	{
+		printf("%s", argv[0]);
+	}
+
+	return (0);
+}
