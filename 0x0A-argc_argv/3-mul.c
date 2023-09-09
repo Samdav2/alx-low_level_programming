a97ca45f0b4e05c5f32c172bd92202cb909115f8
Samdav2 <dawodujohn1@gmail.com>
2023-09-09 12:07:30 +0100

    This multiplies two value of the integer together

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Multiplies to number together
+ * @argc: Count the arguements
+ * @argv: The  vector of the arguement
+ * Return: Nothing
+ **/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		exit(1);
+	}
+
+	else
+	printf("%d", atoi(argv[1]) * atoi(argv[2]));
+}
