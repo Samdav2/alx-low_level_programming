4077e8b5ecf4e78a0245910c2fb0b9505beffcc2
samdav <dawodujohn1@gmail.com>
2023-10-28 05:13:39 +0100

    C program that prints the number needed to change  currency

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C program that prints the amount needed to change a currency
+ * @argc: Arguement counter
+ * @argv: Arguement vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
+
+    int cents = atoi(argv[1]);
+
+    if (cents < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    int coins[] = {25, 10, 5, 2, 1};
+    int numCoins = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        while (cents >= coins[i])
+	{
+            cents -= coins[i];
+            numCoins++;
+        }
+    }
+
+    printf("%d\n", numCoins);
+
+    return (0);
+}
+
