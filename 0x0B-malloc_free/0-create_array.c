6c316fc52a5199f1cb1a4c0abf943c765bb19673
samdav <dawodujohn1@gmail.com>
2023-10-30 15:02:51 +0100

    C program that create a array and allocate memory to them

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,8 @@ char *create_array(unsigned int size, char c)
 
 	unsigned int i = 0;
 
+	create_array = malloc(size + 1) * sizeof(char);
+
 
 	for (i = 0; i < size; i++)
 	{
@@ -24,6 +26,8 @@ char *create_array(unsigned int size, char c)
 
 		printf("0x%02x", c);
 	}
+
+	free(create_array);
 
 	return (0);
 }
