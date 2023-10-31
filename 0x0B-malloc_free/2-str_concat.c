6af276024e92e4fa3634208ad7d30f1f4211609c
samdav <dawodujohn1@gmail.com>
2023-10-31 12:26:14 +0100

    C program that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,10 +7,10 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int lenght;
+	unsigned int lenght;
 	char *pointer;
-	int i;
-	int j;
+	unsigned int i;
+	unsigned int j;
 
 	lenght = (strlen(s1) + strlen(s2));
 	if (s1 == NULL)
