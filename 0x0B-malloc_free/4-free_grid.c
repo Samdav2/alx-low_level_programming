ff6cb2b012f1e5573ceb335654e5dd85cc0a2325
samdav <dawodujohn1@gmail.com>
2023-10-31 15:34:20 +0100

    C program that frees the grid 2d array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_grid - C function that frees the grid arrays
+ * @grid: The pointer to the grid
+ * @height: The height of the grid
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i <= height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
