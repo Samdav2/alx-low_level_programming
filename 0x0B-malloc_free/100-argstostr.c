5fe0e0e5906ecea5008ff3a43522d82241e3df04
samdav <dawodujohn1@gmail.com>
2023-11-01 04:00:53 +0100

    C program that concatenate the arguement of a string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * argstostr - c function that concatenates all the arguement of your program
+ * @ac: Arguement counter
+ * @av: Arguement vector
+ * Return: The concatenate arguements
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char **pointer;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	pointer = (char *)malloc((i + 1) * sizeof(char));
+
+	for (i = 0; i <= ac; i++)
+	{
+		if (!pointer[i] = av[i])
+		{
+			return (NULL);
+		}
+		pointer[i] = av[i];
+		return (pointer[i]);
+	}
+}
