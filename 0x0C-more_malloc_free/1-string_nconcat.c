b5334c54150581a5a239236ce4434f78dcb9f328
samdav <dawodujohn1@gmail.com>
2023-11-02 20:40:32 +0100

    C program that concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,6 +4,7 @@
  * @s1: The first string
  * @s2: The second string
  * @n: The number of words in the string to be copied
+ * Return: A pointer to the concatenate string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
