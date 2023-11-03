ddd7e0347a4e8197a5b6458853402b6f24113699
samdav <dawodujohn1@gmail.com>
2023-11-03 11:08:05 +0100

    C program that allocate memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _calloc - C program that allocate memory for an array
+ * @nmemb: The array
+ * @size: The size of the array
+ * Return: pointer to to the memory allocated
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int n;
+
+	int *pointer;
+
+	for(i = 0; i <= nmemb)
+	{
+		for(n = 0; n <= size)
+		{
+			n++;
+		}
+		i++;
+	}
+	pointer = malloc((n * i) * sizeof(int));
+
+	 return (pointer);
+}
