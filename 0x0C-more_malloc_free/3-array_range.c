0c91c9de9b86f2dbd566d44a94f1db7629d02306
samdav <dawodujohn1@gmail.com>
2023-11-03 12:55:57 +0100

    c program that create a array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,16 +9,13 @@ int *array_range(int min, int max)
 {
 	int i;
 	int *pointer;
-	int size;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	size = (max - min) + 1;
-
-	pointer = malloc((size) * sizeof(int));
+	pointer = malloc((max + 1) * sizeof(int));
 
 	if (pointer == NULL)
 	{
