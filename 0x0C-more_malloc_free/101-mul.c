ef55faaebe95e1b59f98561141aad39f41c92db8
samdav <dawodujohn1@gmail.com>
2023-11-04 05:41:47 +0100

    C program that multiplies two number together

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * main - C program that multiplies two positive number
+ * @argv: Arguement vector
+ * @argc: Arguement Counter
+ * Return: The the result in integers
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int num1, num2;
+	int *mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		if(!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul =  num1 * num2;
+
+	return (mul);
+}
