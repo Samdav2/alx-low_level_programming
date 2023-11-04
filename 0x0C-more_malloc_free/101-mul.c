cd9de60b9cd8b38a18b60dccd103828416cf46e1
samdav <dawodujohn1@gmail.com>
2023-11-04 22:19:21 +0100

    C program that multiplies two number together

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,26 @@
 #include "main.h"
 /**
+ * isDigit - C program that check if an arguement is an integer or not
+ * @str: a pointer to a string
+ * Return: an int
+ */
+
+int isDigit(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+
+/**
  * main - C program that multiplies two positive number
  * @argv: Arguement vector
  * @argc: Arguement Counter
@@ -18,13 +39,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	for (i = 0; i < argc; i++)
+	for (i = 0; i <= 2;)
 	{
-		if (atoi(argv[1]) < 0 || atoi(argv[1]) > 9)
+		if (!isDigit(argv[1]) || !isDigit(argv[2]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
+		i++;
 	}
 
 	num1 = atoi(argv[1]);
