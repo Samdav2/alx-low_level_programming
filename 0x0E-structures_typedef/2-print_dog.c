805a0bb8bf00950016b9b2e2086b5597cec13a8e
samdav <dawodujohn1@gmail.com>
2023-11-07 21:02:18 +0100

    C program that initialize a structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+/**
+ * print_dog - C program that prints a struct dog
+ * @d: A  pointer to the structure
+ */
+void print_dog(struct dog *d)
+{
+
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+
+	if (d->age != 0)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+		
+}
