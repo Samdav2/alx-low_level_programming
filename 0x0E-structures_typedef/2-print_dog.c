632954b2fa9c6863979e37acef1a871ecffe2993
samdav <dawodujohn1@gmail.com>
2023-11-07 21:18:09 +0100

    C program that initialize a structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,30 +5,22 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d)
+	{
 
-	if (d->name != NULL)
-	{
+	if (d->name)
 		printf("Name: %s\n", d->name);
-	}
 	else
-	{
 		printf("Name: (nil)\n");
-	}
 
 	if (d->age >= 0)
-	{
-		printf("Age: %f\n", d->age);
-	}
+		printf("Age: %.06f\n", d->age);
 	else
-	{
 		printf("Age: (nil)\n");
-	}
-	if (d->owner != NULL)
-	{
+
+	if (d->owner)
 		printf("Owner: %s", d->owner);
-	}
 	else
-	{
 		printf("Owner: (nil)\n");
 	}
 		
