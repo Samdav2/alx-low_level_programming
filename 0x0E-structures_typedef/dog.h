6844d0a8ac18e7c04dc3062408562ee5bc2232aa
samdav <dawodujohn1@gmail.com>
2023-11-07 15:09:38 +0100

    C program that creates a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * dog - C program that  create structure for a c program
+ * @name: The name of the user
+ * @age: The age of the user
+ * @owner: The owner of the dog
+ * Return: Nothing
+ */
+
 struct dog
 {
 	char *name;
