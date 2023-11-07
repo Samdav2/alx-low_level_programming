19715e039e6f3d132598d009e93b48869ead8047
samdav <dawodujohn1@gmail.com>
2023-11-07 17:16:21 +0100

    C program that initialize a structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - C program that initialize of type struct dog
+ * @d: A pointer to the structure
+ * @name: The name in the struct
+ * @age: The age in the structure
+ * @wner: The name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
