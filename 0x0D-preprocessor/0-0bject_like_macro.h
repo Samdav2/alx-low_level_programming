e0cdeeb7281f085a456c32465eb0c5d4b32771b5
samdav <dawodujohn1@gmail.com>
2023-11-08 05:14:20 +0100

    C program that create an header files SIZE

---
+++ 0x0D-preprocessor/0-0bject_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef __0BJECT_LIKE_MACRO_H__
+#define __OBJECT_LIKE_MACRO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This files contians all the function meaning that each c files performs here.
