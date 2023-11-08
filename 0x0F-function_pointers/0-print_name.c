ca5af3032f9929b02d6dcd31cbeacf590e9f39e2
samdav <dawodujohn1@gmail.com>
2023-11-08 19:10:19 +0100

    C program that print name through function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - C function that print a name
  * @name: A pointer to the name to be printed
- * @f: A function pointer to the 
+ * @f: A function pointer to the
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
