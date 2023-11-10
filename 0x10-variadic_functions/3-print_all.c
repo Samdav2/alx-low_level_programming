1881fec2807501c46a8b1225ab199f87b24e35ff
samdav <dawodujohn1@gmail.com>
2023-11-10 06:00:32 +0100

    C program that prints different type of argument

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,8 @@
+#include "variadic_functions.h"
+/**
+ * print_all - c program that prints different type of argurment
+ * @format: The format of the argumen
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
