58a1f392c1e42aff3ac81107157f18553a11f11a
samdav <dawodujohn1@gmail.com>
2023-11-12 16:39:01 +0100

    C function that prints the the arguement inputted  in it

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,4 +5,43 @@
  * Return: Nothing
  */
 void print_all(const char * const format, ...)
-{}
+{
+	va_list pointer;
+	int i = 0;
+	char *str;
+
+	va_start(pointer, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(pointer, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(pointer, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(pointer, double));
+				break;
+			case 's': 
+				str = va_arg(pointer, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+			default:
+				break;
+		}
+
+		if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+
+		i++;
+	}
+
+	printf("\n");
+	va_end(pointer);
+}
