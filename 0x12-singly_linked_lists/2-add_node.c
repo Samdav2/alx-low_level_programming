5a705623101d5e798590aebbc345c44997e696f1
samdav <dawodujohn1@gmail.com>
2023-11-16 20:14:10 +0100

    C program that adds new nodes together

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_node - c program that adds nodes togehter
+ * @head: a double pointer to head
+ * @str: a pointer to the string
+ * Return: The value of the node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	n_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	n_node->str = strdup(str);
+	if (n_node->str == NULL)
+	{
+		free(n_node);
+		return (NULL);
+	}
+
+	n_node->len = strlen(str);
+	n_node->next = *head;
+	*head = n_node;
+
+	return (n_node);
+
+}
