935e269678362cb77c5df65daa7e8959969afb4d
samdav <dawodujohn1@gmail.com>
2023-11-16 23:07:56 +0100

    C program that print a name assembly language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    hello db 'Hello, Holberton', 0
+
+section .text
+    global main
+
+extern printf
+
+main:
+    push hello
+    call printf
+    add rsp, 8  ; Adjust the stack pointer
+
+    ; Exit the program
+    mov rdi, 0  ; Return 0 from main
+    call exit
