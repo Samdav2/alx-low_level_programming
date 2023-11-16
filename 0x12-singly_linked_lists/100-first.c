9cbc57160685c1f9ff00e7c717d197cc8cc5726e
samdav <dawodujohn1@gmail.com>
2023-11-16 22:44:16 +0100

    C progrAM that adds a program first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * printfirst - c program that prints a text first
+ * Return: Nothing
+ */
+void printfirst(void)
+{
+	printf("You're beat! and yet, you must allow, \n",
+			"I bore my house upon my back!\n");
+}
