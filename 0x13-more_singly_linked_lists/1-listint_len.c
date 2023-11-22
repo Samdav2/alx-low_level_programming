050a93801485a9049bd05aafbebb1fc409a8289e
samdav <dawodujohn1@gmail.com>
2023-11-22 05:23:00 +0100

    C program that print the number of element in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * listint_len - A c program Returns the number of element in a listint_t list
+ * @h: A pointer to the head of the list
+ * Return: The number of element in  th list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node_counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_counter++;
+	}
+
+	return (node_counter);
+}
